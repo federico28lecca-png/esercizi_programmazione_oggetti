ee2e496eb11587ce24644d96d06c92bce1622951
federico28lecca-png <federico28.lecca@gmail.com>
2026-03-19 16:38:14 +0100

    Secondo push

--- lezione5_es1.py
+++ lezione5_es1.py
@@ -1,10 +1,12 @@
 def min_max_lista(lista):
-    minimo=lista
-    massimo=lista
     minimo=min(lista)
     massimo=max(lista)
     return minimo,massimo
 
-l=[5,3,2,4,6,7,89,0,-32]
+l=[]
+n=int(input("Numero di elementi da inserire: "))
+for i in range(n):
+    num=(float(input("Numero "+str(i+1)+": ")))
+    l+=[num]
 minimo,massimo=min_max_lista(l)
 print("Max: ",massimo,"\nMin: ",minimo)
